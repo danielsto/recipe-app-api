4c10c794650dc1926eaa6c6eb1c258d62d123333
danielsto <danielsto@protonmail.com>
2020-09-23 16:51:00 +0100

    Added TDD case example

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,3 +1,7 @@
 def add(x, y):
-    """Add two numbers together"""
+    """Add two numbers together and return the value"""
     return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return the value"""
+    return x - y
