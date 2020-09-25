2e1ec4341d9f5e67c4011d705db17ed6ee495e1d
danielsto <danielsto@protonmail.com>
2020-09-25 18:46:14 +0100

    Going to W10

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -14,7 +14,7 @@ class AdminSiteTests(TestCase):
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
             email='regular@domain.com',
-            passwrod='password123',
+            password='password123',
             name='Test user full name'
         )
 
@@ -25,4 +25,12 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
-        
+
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # /admin/core/user/
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
