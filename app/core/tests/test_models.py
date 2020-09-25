1dc7df651fc1e5b3c382d20fb6156e88175ef098
danielsto <danielsto@protonmail.com>
2020-09-25 15:20:56 +0100

    Added custom user and superusers with email validation

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,10 +15,26 @@ class ModelTest(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-    
+
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = "testemail@DOMAINUPPERCASE.COM"
         user = get_user_model().objects.create_user(email, "test123")
 
-        self.assertEqual(user.email, email.loer())
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "test123")
+
+
+    def test_create_new_superuser(self):
+        """Test that a new superuser is created"""
+        user = get_user_model().objects.create_superuser(
+            "testemail@domain.com",
+            "test123"
+            )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
