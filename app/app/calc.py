d19057815fca8dce3c6e3175aaca8c03006715c9
danielsto <danielsto@protonmail.com>
2020-09-30 17:08:02 +0100

    Deleted wrong file

--- app/app/calc.py
+++
@@ -1,8 +0,0 @@
-def add(x, y):
-    """Add two numbers together and return the value"""
-    return x + y
-
-
-def subtract(x, y):
-    """Subtract x from y and return the value"""
-    return x - y
